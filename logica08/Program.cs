f3f8cb4645e74263909993c3bf395a5599f8c531
JOSE ELIAS HERMINIO MENDES <jose.ehmendes@senacsp.edu.br>
2025-01-29 16:04:55 -0300

    feat: Matrizes

--- logica08/Program.cs
+++ logica08/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace logica08
 {
@@ -10,7 +6,44 @@ namespace logica08
     {
         static void Main(string[] args)
         {
+            // Declaração e inicialização de uma matriz 3x3
+            int[,] matriz2D = new int[,]
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+            };
 
+            // Com laço For each
+            Console.WriteLine("Com laço For each:");
+            foreach (var valor in matriz2D)
+            {
+                Console.Write($"{valor} ");
+            }
+            Console.WriteLine();
+
+            // Com laço for
+            Console.WriteLine("\nCom laço For:");
+            for (int i = 0; i < matriz2D.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz2D.GetLength(1); j++)
+                {
+                    Console.Write($"{matriz2D[i, j]} ");
+                }
+            }
+            Console.WriteLine();
+
+            // Declaração e inicialização de uma matriz 1D
+            int[] matriz1D = new int[5] { 10, 20, 30, 40, 50 };
+
+            // Exibindo os valores da matriz 1D
+            Console.WriteLine("\nExibindo os valores da matriz 1D:");
+            for (int i = 0; i < matriz1D.Length; i++)
+            {
+                Console.Write($"{matriz1D[i]} ");
+            }
+
+            Console.ReadLine();
         }
     }
 }
